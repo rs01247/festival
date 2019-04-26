42c2eea2ac508de1deabb8cc2449856101733389
Raymond <rs01247@gmail.com>
2019-04-25 21:03:46 -0700

    added axios, working on implicit grant for spotify api

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 // require("dotenv").config();
 const express = require("express");
+const axios = require("axios");
 const bodyParser = require("body-parser");
 const jwt = require("express-jwt");
 const path = require("path");
 const morgan = require("morgan");
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 8888;
+
+app.use(express.static(__dirname + "/public"));
 
 app.use(morgan("dev"))
 
@@ -17,3 +20,4 @@ app.use(bodyParser.json());
 app.listen(PORT, function() {
     console.log(`Listening to ${PORT}`);
 });
+
